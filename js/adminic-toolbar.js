b9945de4cf0dca8683608ecf00ed79cbf3f6959c
Jakub Hnilička <ahoj@jakubhnilicka.cz>
2018-03-02 21:04:02 +0100

    Optimize templates

--- js/adminic-toolbar.js
+++ js/adminic-toolbar.js
@@ -1,52 +1,51 @@
 (function ($, Drupal) {
 
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.adminicToolbar = {
-        attach: function (context) {
-            var $toolbarSecondary = $('.toolbar__secondary');
-            var $body = $('body');
+  Drupal.behaviors.adminicToolbar = {
+    attach: function (context) {
+      var $toolbarSecondary = $('.toolbar__secondary');
+      var $body = $('body');
 
-            if ($('.toolbar_secondary_wrapper.active').length > 0) {
-                showSecondaryToolbar();
-            }
-            else {
-                hideSecondaryToolbar()
-            }
+      if ($('.wrapper.active').length > 0) {
+        showSecondaryToolbar();
+      }
+      else {
+        hideSecondaryToolbar()
+      }
 
-            $('.nano').nanoScroller();
+      $('.nano').nanoScroller();
 
-            $('.toolbar__primary a').not('.dropdown a').on('click', function (e) {
-                $('.toolbar__primary a').removeClass('active');
+      $('.tab').on('click', function (e) {
+        $('.tab').removeClass('active');
 
-                var $tab = $(this);
-                var tabId = $tab.attr('id');
-                var tabKey = tabId.substring(4);
-                var $sectionWrapper = $('#toolbar-' + tabKey);
-                $tab.addClass('active');
+        var $tab = $(this);
+        var tabId = $tab.attr('id');
+        var tabKey = tabId.substring(4);
+        var $sectionWrapper = $('#toolbar-' + tabKey);
+        $tab.addClass('active');
+        $('.wrapper.active').removeClass('active');
 
-                if ($sectionWrapper[0] !== undefined) {
-                    e.preventDefault();
-                    $('.toolbar_secondary_wrapper.active').removeClass('active');
-                    $sectionWrapper.addClass('active');
-                    showSecondaryToolbar();
-                }
-                else {
-                    $('.toolbar_secondary_wrapper.active').removeClass('active');
-                    hideSecondaryToolbar()
-                }
-            });
-
-            function showSecondaryToolbar() {
-                $toolbarSecondary.show();
-                $body.addClass('adminic-toolbar-secondary');
-            }
-
-            function hideSecondaryToolbar() {
-                $toolbarSecondary.hide();
-                $body.removeClass('adminic-toolbar-secondary');
-            }
+        if ($sectionWrapper[0] !== undefined) {
+          e.preventDefault();
+          $sectionWrapper.addClass('active');
+          showSecondaryToolbar();
         }
-    };
+        else {
+          hideSecondaryToolbar()
+        }
+      });
+
+      function showSecondaryToolbar() {
+        $toolbarSecondary.show();
+        $body.addClass('adminic-toolbar-secondary');
+      }
+
+      function hideSecondaryToolbar() {
+        $toolbarSecondary.hide();
+        $body.removeClass('adminic-toolbar-secondary');
+      }
+    }
+  };
 
 }(jQuery, Drupal));
