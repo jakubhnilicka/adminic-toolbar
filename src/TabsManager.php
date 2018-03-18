3d65b43f3f366526cd712e7522f8a113d5e29d41
Jakub Hnilička <ahoj@jakubhnilicka.cz>
2018-03-18 08:47:34 +0100

    Refactor Tabs

--- src/TabsManager.php
+++ src/TabsManager.php
@@ -18,6 +18,17 @@ use Exception;
  */
 class TabsManager {
 
+  const YML_TABS_KEY = 'tabs';
+  const YML_TAB_WEIGHT_KEY = 'weight';
+  const YML_TAB_PRESET_KEY = 'set';
+  const YML_TABS_ID_KEY = 'id';
+  const YML_TABS_PRIMARY_SECTION_KEY = 'widget_id';
+  const YML_TABS_ROUTE_NAME_KEY = 'route';
+  const YML_TABS_ROUTE_PARAMETERS_KEY = 'route_params';
+  const YML_TABS_TITLE_KEY = 'title';
+  const YML_TABS_DISABLED_KEY = 'disabled';
+  const YML_TABS_BADGE_KEY = 'badge';
+
   /**
    * Discovery manager.
    *
@@ -73,6 +84,117 @@ class TabsManager {
   }
 
   /**
+   * Get all defined tabs from all config files.
+   */
+  protected function parseTabs() {
+    $config = $this->discoveryManager->getConfig();
+
+    $weight = 0;
+    $configTabs = [];
+    foreach ($config as $configFile) {
+      if (isset($configFile[self::YML_TABS_KEY])) {
+        foreach ($configFile[self::YML_TABS_KEY] as $tab) {
+          $tab[self::YML_TAB_WEIGHT_KEY] = isset($tab[self::YML_TAB_WEIGHT_KEY]) ? $tab[self::YML_TAB_WEIGHT_KEY] : $weight++;
+          $tab[self::YML_TAB_PRESET_KEY] = isset($tab[self::YML_TAB_PRESET_KEY]) ? $tab[self::YML_TAB_PRESET_KEY] : 'default';
+          $key = $tab[self::YML_TABS_ID_KEY];
+          $configTabs[$key] = $tab;
+        }
+      }
+    }
+
+    // Sort tabs by weight.
+    uasort($configTabs, 'Drupal\Component\Utility\SortArray::sortByWeightElement');
+
+    // Call hook alters.
+    $this->moduleHandler->alter('toolbar_config_tabs', $configTabs);
+
+    // Add tabs.
+    $this->addTabs($configTabs);
+  }
+
+  /**
+   * Add tabs.
+   *
+   * @param array $configTabs
+   *   Array of tabs.
+   */
+  protected function addTabs(array $configTabs) {
+    foreach ($configTabs as $tab) {
+
+      $this->validateTab($tab);
+
+      $id = $tab[self::YML_TABS_ID_KEY];
+      $widget_id = $tab[self::YML_TABS_PRIMARY_SECTION_KEY];
+      $route = $tab[self::YML_TABS_ROUTE_NAME_KEY];
+      $route_params = isset($tab[self::YML_TABS_ROUTE_PARAMETERS_KEY]) ? $tab[self::YML_TABS_ROUTE_PARAMETERS_KEY] : [];
+      $isValid = $this->routeManager->isRouteValid($route, $route_params);
+
+      if ($isValid && $tab[self::YML_TAB_PRESET_KEY] == $this->discoveryManager->getActiveSet()) {
+        $title = isset($tab[self::YML_TABS_TITLE_KEY]) ? $tab[self::YML_TABS_TITLE_KEY] : $this->routeManager->getDefaultTitle($route, $route_params);
+        $title = empty($title) ? '' : $title;
+        $url = Url::fromRoute($route, $route_params);
+        $disabled = isset($tab[self::YML_TABS_DISABLED_KEY]) ? $tab[self::YML_TABS_DISABLED_KEY] : FALSE;
+        $badge = isset($tab[self::YML_TABS_BADGE_KEY]) ? $tab[self::YML_TABS_BADGE_KEY] : '';
+        $active = FALSE;
+        $this->addTab(new Tab($id, $widget_id, $url, $title, $active, $disabled, $badge));
+      }
+    }
+  }
+
+  /**
+   * Add tab.
+   *
+   * @param \Drupal\adminic_toolbar\Tab $tab
+   *   Tab.
+   */
+  public function addTab(Tab $tab) {
+    $key = $this->getTabKey($tab);
+    $this->tabs[$key] = $tab;
+    // Remove tab if exists and is disabled.
+    if (isset($this->tabs[$key]) && $tab->isDisabled()) {
+      unset($this->tabs[$key]);
+    }
+  }
+
+  /**
+   * Validate tab required parameters.
+   *
+   * @param array $tab
+   *   Tab array.
+   */
+  protected function validateTab(array $tab) {
+    try {
+      $obj = json_encode($tab);
+      if (!isset($tab[self::YML_TABS_ID_KEY])) {
+        throw new Exception('Tab ID parameter missing ' . $obj);
+      };
+      if (!isset($tab[self::YML_TABS_PRIMARY_SECTION_KEY])) {
+        throw new Exception('Tab widget_id parameter missing ' . $obj);
+      };
+      if (!isset($tab[self::YML_TABS_ROUTE_NAME_KEY])) {
+        throw new Exception('Tab route parameter missing ' . $obj);
+      }
+    }
+    catch (Exception $e) {
+      print $e->getMessage();
+    }
+  }
+
+  /**
+   * Get tabs.
+   *
+   * @return array
+   *   Return array of tabs.
+   */
+  public function getTabs() {
+    if (empty($this->tabs)) {
+      $this->parseTabs();
+    }
+
+    return $this->tabs;
+  }
+
+  /**
    * Add tab to active tabs.
    *
    * @param \Drupal\adminic_toolbar\Tab $tab
@@ -107,50 +229,6 @@ class TabsManager {
   }
 
   /**
-   * Get tabs.
-   *
-   * @return array
-   *   Return array of tabs.
-   */
-  public function getTabs() {
-    if (empty($this->tabs)) {
-      $this->parseTabs();
-    }
-
-    return $this->tabs;
-  }
-
-  /**
-   * Get all defined tabs from all config files.
-   */
-  protected function parseTabs() {
-    $config = $this->discoveryManager->getConfig();
-
-    $weight = 0;
-    $configTabs = [];
-    foreach ($config as $configFile) {
-      if (isset($configFile['tabs'])) {
-        foreach ($configFile['tabs'] as $tab) {
-          $tab['weight'] = isset($tab['weight']) ? $tab['weight'] : $weight;
-          $tab['set'] = isset($tab['set']) ? $tab['set'] : 'default';
-          $key = $tab['id'];
-          $configTabs[$key] = $tab;
-          $weight++;
-        }
-      }
-    }
-
-    // Sort tabs by weight.
-    uasort($configTabs, 'Drupal\Component\Utility\SortArray::sortByWeightElement');
-
-    // Call hook alters.
-    $this->moduleHandler->alter('toolbar_config_tabs', $configTabs);
-
-    // Add tabs.
-    $this->addTabs($configTabs);
-  }
-
-  /**
    * Get first active tab.
    *
    * @return \Drupal\adminic_toolbar\Tab
@@ -163,75 +241,6 @@ class TabsManager {
     }
 
     return NULL;
-  }
-
-  /**
-   * Add tabs.
-   *
-   * @param array $configTabs
-   *   Array of tabs.
-   */
-  protected function addTabs(array $configTabs) {
-    foreach ($configTabs as $tab) {
-
-      $this->validateTab($tab);
-
-      $id = $tab['id'];
-      $widget_id = $tab['widget_id'];
-      $route = $tab['route'];
-      $route_params = isset($tab['route_params']) ? $tab['route_params'] : [];
-      $isValid = $this->routeManager->isRouteValid($route, $route_params);
-
-      if ($isValid && $tab['set'] == $this->discoveryManager->getActiveSet()) {
-        $title = isset($tab['title']) ? $tab['title'] : $this->routeManager->getDefaultTitle($route, $route_params);
-        $title = empty($title) ? '' : $title;
-        $url = Url::fromRoute($route, $route_params);
-        $disabled = isset($tab['disabled']) ? $tab['disabled'] : FALSE;
-        $badge = isset($tab['badge']) ? $tab['badge'] : '';
-        $active = FALSE;
-        $this->addTab(new Tab($id, $widget_id, $url, $title, $active, $disabled, $badge));
-      }
-    }
-  }
-
-  /**
-   * Add tab.
-   *
-   * @param \Drupal\adminic_toolbar\Tab $tab
-   *   Tab.
-   */
-  public function addTab(Tab $tab) {
-    $key = $this->getTabKey($tab);
-    $this->tabs[$key] = $tab;
-    // Remove tab if exists and is disabled.
-    if (isset($this->tabs[$key]) && $tab->isDisabled()) {
-      unset($this->tabs[$key]);
-    }
-
-  }
-
-  /**
-   * Validate tab required parameters.
-   *
-   * @param array $tab
-   *   Tab array.
-   */
-  protected function validateTab(array $tab) {
-    try {
-      $obj = json_encode($tab);
-      if (!isset($tab['id'])) {
-        throw new Exception('Tab ID parameter missing ' . $obj);
-      };
-      if (!isset($tab['widget_id'])) {
-        throw new Exception('Tab widget_id parameter missing ' . $obj);
-      };
-      if (!isset($tab['route'])) {
-        throw new Exception('Tab route parameter missing ' . $obj);
-      }
-    }
-    catch (Exception $e) {
-      print $e->getMessage();
-    }
   }
 
 }
